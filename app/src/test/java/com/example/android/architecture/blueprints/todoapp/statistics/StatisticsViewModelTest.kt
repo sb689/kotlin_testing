c9efabec75cdfbc447628786e7c4d181bdd755ec
Sujata Baral <sujata689@gmail.com>
2021-02-12 15:13:12 -0600

    statisticsViewModelTest with dispatcher timing

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+
+class StatisticsViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var statisticsViewModel: StatisticsViewModel
+
+    private lateinit var tasksRepository: FakeTestRepository
+
+    @Before
+    fun setupStatisticsViewModel() {
+        tasksRepository = FakeTestRepository()
+        statisticsViewModel = StatisticsViewModel(tasksRepository)
+    }
+
+    @Test
+    fun loadTasks_loading() {
+        mainCoroutineRule.pauseDispatcher()
+        statisticsViewModel.refresh()
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(true))
+        mainCoroutineRule.resumeDispatcher()
+
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(false))
+    }
+
+}

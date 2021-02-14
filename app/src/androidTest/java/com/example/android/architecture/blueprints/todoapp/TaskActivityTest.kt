d77337b0e8c43369f6880a0fb002916e5bff1f0a
Sujata Baral <sujata689@gmail.com>
2021-02-13 18:34:53 -0600

    DatabindingIdlingResource implement

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TaskActivityTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TaskActivityTest.kt
@@ -4,8 +4,8 @@ import android.service.autofill.Validators.not
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 
@@ -15,6 +15,9 @@ import androidx.test.filters.LargeTest
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
 import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
+import com.example.android.architecture.blueprints.todoapp.util.DataBindingIdlingResource
+import com.example.android.architecture.blueprints.todoapp.util.EspressoIdlingResource
+import com.example.android.architecture.blueprints.todoapp.util.monitorActivity
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
@@ -30,6 +33,20 @@ import org.hamcrest.core.IsNot.not
 class TaskActivityTest {
 
     private lateinit var repository: TasksRepository
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
 
     @Before
     fun init() {
@@ -52,6 +69,7 @@ class TaskActivityTest {
 
         // Start up Tasks screen.
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
 
         onView(withText("TITLE1")).perform(click())
         onView(withId(R.id.task_detail_title_text)).check(matches(withText("TITLE1")))
@@ -71,6 +89,34 @@ class TaskActivityTest {
 
         // Make sure the activity is closed before resetting the db:
         activityScenario.close()
+    }
+
+    @Test
+    fun createOneTask_deleteTask() {
+
+        // 1. Start TasksActivity.
+        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // 2. Add an active task by clicking on the FAB and saving a new task.
+        onView(withId(R.id.add_task_fab)).perform(click())
+        onView(withId(R.id.add_task_title_edit_text)).perform(typeText("adding TITLE"))
+        onView(withId(R.id.add_task_description_edit_text)).perform(typeText("adding DESCRIPTION"))
+        onView(withId(R.id.save_task_fab)).perform(click())
+
+        //verifying that the the added task is displayed on main task list
+        onView(withText("adding TITLE")).check(matches(isDisplayed()))
+        // 3. Open the new task in a details view.
+        onView(withText("adding TITLE")).perform(click())
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("adding TITLE")))
+
+        // 4. Click delete task in menu.
+        onView(withId(R.id.menu_delete)).perform(click())
+        // 5. Verify it was deleted.
+        onView(withText("adding TITLE")).check(doesNotExist())
+        // 6. Make sure the activity is closed.
+        activityScenario.close()
+
     }
 
 }
